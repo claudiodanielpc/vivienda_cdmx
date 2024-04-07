2003a881e64539bb1bfb9415575a57bed1597582
maestrokarin <claudio@comunidad.unam.mx>
2024-04-07 11:29:04 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vivienda_cdmx',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,  
+    url='https://github.com/claudiodanielpc/vivienda_cdmx',
+    author='Claudio Daniel Pacheco-Castro',
+    author_email='claudio@comunidad.unam.mx')
